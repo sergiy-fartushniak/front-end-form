5b33eb248872bf03aa74f90db2bd850d02b1d27f
Sergiy Fartushniak <fartux97@gmail.com>
2021-08-17 12:24:53 +0300

    form rebuild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { FormComponent } from './form/form.component';
     FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    NoopAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  ngOnInit(): void {}
+  name = '';
+
+  inputHandler(event: any) {
+    this.name = event.target.value
+    console.log(this.name);
   }
-
 }
